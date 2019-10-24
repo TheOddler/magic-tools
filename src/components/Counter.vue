1d3e8f29873b8a85f8cbb2a184af611d63ea7f05
Pablo Bollansée <pablo.bollansee@gmail.com>
2019-10-24 15:20:17 +0200

    Initial commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,35 @@
+<template>
+  <div v-hammer:swipe="onSwipe">{{ count }}</div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      count: 0
+    };
+  },
+  props: {
+    label: String
+  },
+  methods: {
+    onSwipe: function(event) {
+      if (event.direction == 8) {
+        //up
+        this.count++;
+      } else if (event.direction == 16) {
+        //down
+        this.count--;
+      }
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+div {
+  background-color: red;
+  height: 150px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import { VueHammer } from 'vue2-hammer'
+import App from './App.vue'
+
+Vue.use(VueHammer);
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
