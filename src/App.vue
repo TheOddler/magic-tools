1811b1865fb77b0d4f1c3b2aed3bcf49d9d75954
Pablo Bollansée <pablo.bollansee@gmail.com>
2019-10-26 13:46:53 +0200

    Automatic scaling + better text

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" :style="{ transform: 'scale(' + scale + ')' }">
     <Coin />
     <Coin />
     <Coin />
@@ -16,19 +16,55 @@ import Die from "./components/Die.vue";
 
 export default {
   name: "app",
+  data: function() {
+    return {
+      scale: 1
+    };
+  },
   components: {
     PlayerField,
     Coin,
     Die
+  },
+  created() {
+    this.updateScale();
+  },
+  mounted: function() {
+    window.addEventListener("resize", this.updateScale);
+  },
+  beforeDestroy: function() {
+    window.removeEventListener("resize", this.updateScale);
+  },
+  methods: {
+    updateScale(event) {
+      window.console.log(
+        "resized: " + window.innerWidth + "; " + window.innerHeight
+      );
+      window.console.log(event);
+      var xScale = window.innerWidth / 1006;
+      var yScale = window.innerHeight / 654;
+      this.scale = Math.min(xScale, yScale);
+    }
   }
 };
 </script>
 
 <style>
+body {
+  overflow: hidden;
+}
+
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+
+  color: white;
+  text-shadow: 0px 0px 5px black, 2px 2px 2px black;
+
+  width: 1006px;
+  height: 654px;
+
+  transform-origin: top left;
 }
 </style>
